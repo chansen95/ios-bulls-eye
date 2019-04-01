7e34df1ebaa5467fafcc8a219c74bbcc364b44e6
Christian Hansen <cplay@optonline.net>
2019-04-01 11:50:10 -0400

    add to githubb

---
+++ BullsEye/AboutViewController.swift
@@ -0,0 +1,26 @@
+//
+//  AboutViewController.swift
+//  BullsEye
+//
+//  Created by Christian Hansen on 1/12/18.
+//  Copyright © 2018 Christian Hansen. All rights reserved.
+//
+
+import UIKit
+
+class AboutViewController: UIViewController {
+
+    @IBAction func close() {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+}
